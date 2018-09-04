0b216a99921d616c7b6b5f6bab36e4bc5c69ef76
IrishSenthilkumar <40599271+IrishSenthilkumar@users.noreply.github.com>
2018-09-04 22:27:03 +0100

    Add files via upload

---
+++ Values and Variables/Control_Codes_within_Strings.py
@@ -0,0 +1,20 @@
+'''
+Created on 4 Sep 2018
+
+@author: Irish
+'''
+
+# newline control code { /n }
+
+print('This is the 1st line')
+print('\nThis is the 3rd line')
+
+# tab control code { /t }
+
+print('This sits flush with the edge\tThis is indented more to the right')
+
+# note the use of double backslash to denote a single backslash in console e.g file directories
+
+directory = 'C:\\Users\\Irish\\randomFileThingy.lol'
+print(directory)
+
