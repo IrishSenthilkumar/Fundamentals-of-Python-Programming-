c0bdaa43f92306dd0e7e75539e900ae29bd6dc28
IrishSenthilkumar <40599271+IrishSenthilkumar@users.noreply.github.com>
2018-09-04 22:27:43 +0100

    Update Control_Codes_within_Strings.py

--- Values and Variables/Control_Codes_within_Strings.py
+++ Values and Variables/Control_Codes_within_Strings.py
@@ -5,16 +5,13 @@ Created on 4 Sep 2018
 '''
 
 # newline control code { /n }
-
 print('This is the 1st line')
 print('\nThis is the 3rd line')
 
 # tab control code { /t }
-
 print('This sits flush with the edge\tThis is indented more to the right')
 
 # note the use of double backslash to denote a single backslash in console e.g file directories
-
 directory = 'C:\\Users\\Irish\\randomFileThingy.lol'
 print(directory)
 
